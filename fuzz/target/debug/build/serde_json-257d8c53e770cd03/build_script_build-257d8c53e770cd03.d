be4cd7dabb3af4fd62ae1761aaf619d7477e26bf
venelouis <venelouistyago@gmail.com>
2026-06-24 12:23:15 -0300

    chore: ignore fuzz/target to resolve scorecard binary artifact alerts

--- fuzz/target/debug/build/serde_json-257d8c53e770cd03/build_script_build-257d8c53e770cd03.d
+++
@@ -1,5 +0,0 @@
-C:\Users\venelouis\Desktop\REPOS\rullst-orm\fuzz\target\debug\build\serde_json-257d8c53e770cd03\build_script_build-257d8c53e770cd03.d: C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_json-1.0.150\build.rs
-
-C:\Users\venelouis\Desktop\REPOS\rullst-orm\fuzz\target\debug\build\serde_json-257d8c53e770cd03\build_script_build-257d8c53e770cd03.exe: C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_json-1.0.150\build.rs
-
-C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_json-1.0.150\build.rs:
