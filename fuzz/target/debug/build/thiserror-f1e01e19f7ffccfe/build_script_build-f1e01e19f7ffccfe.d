d478c17640f9ecb2f8ac0e0db87cbeacd045e659
venelouis <venelouistyago@gmail.com>
2026-06-24 12:57:38 -0300

    chore: remove fuzz/target from git and fix .gitignore

--- fuzz/target/debug/build/thiserror-f1e01e19f7ffccfe/build_script_build-f1e01e19f7ffccfe.d
+++
@@ -1,5 +0,0 @@
-C:\Users\venelouis\Desktop\REPOS\rullst-orm\fuzz\target\debug\build\thiserror-f1e01e19f7ffccfe\build_script_build-f1e01e19f7ffccfe.d: C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\thiserror-2.0.18\build.rs
-
-C:\Users\venelouis\Desktop\REPOS\rullst-orm\fuzz\target\debug\build\thiserror-f1e01e19f7ffccfe\build_script_build-f1e01e19f7ffccfe.exe: C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\thiserror-2.0.18\build.rs
-
-C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\thiserror-2.0.18\build.rs:
--- fuzz/target/debug/deps/jobserver-dfde4d678e64beb2.d
+++
@@ -1,9 +0,0 @@
-C:\Users\venelouis\Desktop\REPOS\rullst-orm\fuzz\target\debug\deps\jobserver-dfde4d678e64beb2.d: C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\lib.rs C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\error.rs C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\windows.rs
-
-C:\Users\venelouis\Desktop\REPOS\rullst-orm\fuzz\target\debug\deps\libjobserver-dfde4d678e64beb2.rlib: C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\lib.rs C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\error.rs C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\windows.rs
-
-C:\Users\venelouis\Desktop\REPOS\rullst-orm\fuzz\target\debug\deps\libjobserver-dfde4d678e64beb2.rmeta: C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\lib.rs C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\error.rs C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\windows.rs
-
-C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\lib.rs:
-C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\error.rs:
-C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\jobserver-0.1.34\src\windows.rs:
