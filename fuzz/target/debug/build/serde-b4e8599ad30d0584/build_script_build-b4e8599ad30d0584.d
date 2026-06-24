fe5303617c123f1048d41453e032fe013a5ba76e
venelouis <venelouistyago@gmail.com>
2026-06-24 12:32:02 -0300

    fix: remove remaining binary artifacts from fuzz/target

--- fuzz/target/debug/build/serde-b4e8599ad30d0584/build_script_build-b4e8599ad30d0584.d
+++
@@ -1,5 +0,0 @@
-C:\Users\venelouis\Desktop\REPOS\rullst-orm\fuzz\target\debug\build\serde-b4e8599ad30d0584\build_script_build-b4e8599ad30d0584.d: C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs
-
-C:\Users\venelouis\Desktop\REPOS\rullst-orm\fuzz\target\debug\build\serde-b4e8599ad30d0584\build_script_build-b4e8599ad30d0584.exe: C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs
-
-C:\Users\venelouis\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs:
