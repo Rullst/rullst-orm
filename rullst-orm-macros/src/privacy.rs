b4652e7a27fa3411aeae53071872100a59bd7533
venelouis <venelouistyago@gmail.com>
2026-07-23 00:06:10 -0300

    ok

--- rullst-orm-macros/src/privacy.rs
+++ rullst-orm-macros/src/privacy.rs
@@ -11,32 +11,32 @@ pub fn derive_personal_data_impl(input: TokenStream) -> TokenStream {
     let mut encrypted_fields = Vec::new();
     let mut has_encrypted_data = false;
 
-    if let Data::Struct(data_struct) = input.data {
-        if let Fields::Named(fields_named) = data_struct.fields {
-            for field in fields_named.named {
-                let field_name = field.ident.unwrap();
+    if let Data::Struct(data_struct) = input.data
+        && let Fields::Named(fields_named) = data_struct.fields
+    {
+        for field in fields_named.named {
+            let field_name = field.ident.unwrap();
 
-                // Check if the field has the `#[privacy]` attribute
-                let has_privacy = field
-                    .attrs
-                    .iter()
-                    .any(|attr| attr.path().is_ident("privacy"));
+            // Check if the field has the `#[privacy]` attribute
+            let has_privacy = field
+                .attrs
+                .iter()
+                .any(|attr| attr.path().is_ident("privacy"));
 
-                if has_privacy {
-                    has_encrypted_data = true;
-                    encrypted_fields.push(field_name.to_string());
-                    // If it's sensitive, mask the output in standard log/debug
-                    debug_fields = quote! {
-                        #debug_fields
-                        .field(stringify!(#field_name), &"[REDACTED_BY_RULLST_SHIELD]")
-                    };
-                } else {
-                    // If it's public, display it normally
-                    debug_fields = quote! {
-                        #debug_fields
-                        .field(stringify!(#field_name), &self.#field_name)
-                    };
-                }
+            if has_privacy {
+                has_encrypted_data = true;
+                encrypted_fields.push(field_name.to_string());
+                // If it's sensitive, mask the output in standard log/debug
+                debug_fields = quote! {
+                    #debug_fields
+                    .field(stringify!(#field_name), &"[REDACTED_BY_RULLST_SHIELD]")
+                };
+            } else {
+                // If it's public, display it normally
+                debug_fields = quote! {
+                    #debug_fields
+                    .field(stringify!(#field_name), &self.#field_name)
+                };
             }
         }
     }
